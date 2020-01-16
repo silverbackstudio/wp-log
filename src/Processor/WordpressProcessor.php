e8acb3eb9ddf91ae4af5878e42e4de689a423990
Brando Meniconi <b.meniconi@silverbackstudio.it>
2020-01-16 11:53:20 +0000

    add site id/url context variables to WordpressPreprocessor

--- src/Processor/WordpressProcessor.php
+++ src/Processor/WordpressProcessor.php
@@ -16,27 +16,30 @@ namespace Svbk\WP\Log\Processor;
  *
  * @author Brando Meniconi <b.meniconi@silverbackstudio.it>
  */
-class WordpressProcessor
-{
+class WordpressProcessor {
 
-    public function __invoke(array $record)
-    {
-        
-        if ( defined('DOING_AJAX') && DOING_AJAX ) {
-            $record['context']['wp_request_type'] = 'ajax';
-        } else if ( is_admin() ) {
-            $record['context']['wp_request_type'] = 'admin';
-        } else {
-            $record['context']['wp_request_type'] = 'frontend';
-        }
-        
-        if ( is_user_logged_in() ) {
-            $user = wp_get_current_user();
-            $record['context']['user_id'] = $user->ID;
-            $record['context']['user_login'] = $user->user_login;
-        }
-        
-        return $record;
-    }
+
+	public function __invoke( array $record ) {
+
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+			$record['context']['wp_request_type'] = 'ajax';
+		} else if ( is_admin() ) {
+			$record['context']['wp_request_type'] = 'admin';
+		} else {
+			$record['context']['wp_request_type'] = 'frontend';
+		}
+
+		$record['context']['siteurl'] = get_site_url();
+		$record['context']['site_id'] = get_current_blog_id();
+		$record['context']['host'] = $_SERVER['HTTP_HOST'];
+
+		if ( is_user_logged_in() ) {
+			$user = wp_get_current_user();
+			$record['context']['user_id'] = $user->ID;
+			$record['context']['user_login'] = $user->user_login;
+		}
+
+		return $record;
+	}
 
 }
